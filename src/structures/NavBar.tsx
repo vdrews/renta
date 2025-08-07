0d662e233b3beaa53b90e77b482bc7c41dd4781e
vdrews <vanessa.andrews876@gmail.com>
2025-08-07 18:44:44 -0500

    added the full navbar

---
+++ src/structures/NavBar.tsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import user from "../assets/user.svg"
+import { AlignLeft } from 'lucide-react';
+
+import { ChevronDown } from 'lucide-react';
+
+const NavBar = () => {
+  return (
+   <>
+
+   
+   <div className="flex items-center gap-x-6  h-[4rem] p-2 ">
+     <AlignLeft className="text-pink-500"/>
+    
+        <input type="text" name='search' id='search' placeholder='search'
+        className="ml-4 bg-gray-100 rounded-sm max-w-l w-[38rem] h-[2rem] text-sm font-semibold text-gray-400  border-gray-500 p-2"/>
+   
+   
+     <div className="relative w-16 h-16 p-1 flex items-center gap-x-1">
+ <Avatar className=" h-8 w-8 object-cover  ring-2 ring-pink-500 shadow-xl/30 ring-offset-2 ring-offset-white  ">
+
+  <AvatarImage src={user} className="object-cover"/>
+  <AvatarFallback>img</AvatarFallback>
+</Avatar>
+
+ <ChevronDown className="text-[#adadad] w-4 h-4 mt-4"/>
+     </div>
+
+
+   </div>
+
+   
+   
+   </>
+  )
+}
+
+export default NavBar
