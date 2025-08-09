1e0bbd6b43260349f47192358461db4fe1fde91c
vdrews <vanessa.andrews876@gmail.com>
2025-08-09 10:50:14 -0500

    create ui display design - grids

---
+++ src/structures/HouseBar.tsx
@@ -0,0 +1,8 @@
+// import React from 'react'
+const HouseBar = () => {
+  return (
+    <div>HouseBar</div>
+  )
+}
+
+export default HouseBar
--- src/structures/NavBar.tsx
+++ src/structures/NavBar.tsx
@@ -2,6 +2,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import user from "../assets/user.svg"
 import { AlignLeft } from 'lucide-react';
+import { Input } from "@/components/ui/input";
 
 import { ChevronDown } from 'lucide-react';
 
@@ -16,7 +17,6 @@ const NavBar = () => {
         <input type="text" name='search' id='search' placeholder='search'
         className="ml-4 bg-gray-100 rounded-sm max-w-l w-[38rem] h-[2rem] text-sm font-semibold text-gray-400  border-gray-500 p-2"/>
    
-   
      <div className="relative w-16 h-16 p-1 flex items-center gap-x-1">
  <Avatar className=" h-8 w-8 object-cover  ring-2 ring-pink-500 shadow-xl/30 ring-offset-2 ring-offset-white  ">
 
@@ -30,8 +30,52 @@ const NavBar = () => {
 
    </div>
 
+
+<div className="grid grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4 ">
+  
+    <div className=""><img src={user} alt="housng img" className="h-50 rounded-lg  p-4 "/></div>
+    {/* // need to hace a fall back square - empty container that says choose img */}
+    
+    <div className=" p-4 overflow-auto max-h-50">
+      <h1>Owner: Kenny Smith</h1>
+      <h2>Addi: Tavi </h2>
+      <h2>Costs: $4 </h2>
+      <p>notes: <Input placeholder="lorem"/> </p>
+    
+    </div>
+    
+    </div>
+   {/* to be components  */}
+
+   <div className="grid grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4 ">
+  
+    <div className=""><img src={user} alt="housng img" className="h-50 rounded-lg  p-4 "/></div>
+    {/* // need to hace a fall back square - empty container that says choose img */}
+    
+    <div className=" p-4 overflow-auto max-h-50">
+      <h1>Owner: Kenny Smith</h1>
+      <h2>Addi: Tavi </h2>
+      <h2>Costs: $4 </h2>
+      <p>notes: <Input placeholder="lorem"/> </p>
+    
+    </div>
+    
+    </div>
    
-   
+   <div className="grid grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4 ">
+  
+    <div className=""><img src={user} alt="housng img" className="h-50 rounded-lg  p-4 "/></div>
+    {/* // need to hace a fall back square - empty container that says choose img */}
+    
+    <div className=" p-4 overflow-auto max-h-50">
+      <h1>Owner: Kenny Smith</h1>
+      <h2>Addi: Tavi </h2>
+      <h2>Costs: $4 </h2>
+      <p>notes: <Input placeholder="lorem"/> </p>
+    
+    </div>
+    
+    </div>
    </>
   )
 }
