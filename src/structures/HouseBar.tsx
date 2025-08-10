805eb5cb5b8f25f73f28181b9d6a10c857867bf9
vdrews <vanessa.andrews876@gmail.com>
2025-08-10 08:49:23 -0500

    (comp): housebar added + removed div and added the card

--- src/structures/HouseBar.tsx
+++ src/structures/HouseBar.tsx
@@ -1,8 +1,92 @@
-// import React from 'react'
+
+
+import user from "../assets/user.svg"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+
 const HouseBar = () => {
+
+  
+
   return (
-    <div>HouseBar</div>
+   <>
+
+<Card  className="grid gap-0 grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4">
+  
+  
+      <div className="p-3"><img src={user} alt="housng img" className="rounded-lg  "/></div>
+      {/* // need to hace a fall back square - empty container that says choose img */}
+  
+ 
+  
+  
+      <CardContent >
+      
+
+
+         <div className="flex flex-col gap-1">
+
+           <div className="flex items-center gap-1">
+    <Label htmlFor="owner" className="w-24">Owner</Label>
+    <Input
+      type="text"
+      id="owner"
+      placeholder="e.g., Jeffery Miller "
+  
+    />
+    </div>
+ <div className="flex items-center gap-1">
+    <Label htmlFor="address" className="w-24">Address</Label>
+    <Input
+      type="text"
+      id="address"
+      placeholder="e.g., Constant Spring"
+  
+    />
+    </div>
+     <div className="flex items-center gap-1">
+    <Label htmlFor="cost" className="w-24">Cost</Label>
+    <Input
+      type="text"
+      id="cost"
+      placeholder="e.g., 4 Mil"
+  
+    />
+    </div>
+     <div className="flex items-center gap-1">
+    <Label htmlFor="notes" className="w-24">Notes</Label>
+    <Textarea
+      
+      id="notes"
+      placeholder="e.g., The place noisy"
+  
+    />
+    </div>
+
+
+
+    
+         </div>
+    </CardContent>
+ 
+  
+      
+</Card>
+
+
+
+
+   {/* to be components  */}
+
+     </>
   )
 }
 
 export default HouseBar
+
+
+// notes : the display should be editable  on click so no forms - click and ten you can change it without moving off screen 
+// create an add screen -- form + change these out to cards -- display 
--- src/structures/NavBar.tsx
+++ src/structures/NavBar.tsx
@@ -1,16 +1,19 @@
-// import React from 'react'
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import user from "../assets/user.svg"
 import { AlignLeft } from 'lucide-react';
-import { Input } from "@/components/ui/input";
-
 import { ChevronDown } from 'lucide-react';
+import HouseBar from "./HouseBar";
+
+  
 
 const NavBar = () => {
+
+  
+
   return (
    <>
 
-   
    <div className="flex items-center gap-x-6  h-[4rem] p-2 ">
      <AlignLeft className="text-pink-500"/>
     
@@ -26,58 +29,23 @@ const NavBar = () => {
 
  <ChevronDown className="text-[#adadad] w-4 h-4 mt-4"/>
      </div>
-
-
    </div>
 
+<HouseBar/>
 
-<div className="grid grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4 ">
-  
-    <div className=""><img src={user} alt="housng img" className="h-50 rounded-lg  p-4 "/></div>
-    {/* // need to hace a fall back square - empty container that says choose img */}
-    
-    <div className=" p-4 overflow-auto max-h-50">
-      <h1>Owner: Kenny Smith</h1>
-      <h2>Addi: Tavi </h2>
-      <h2>Costs: $4 </h2>
-      <p>notes: <Input placeholder="lorem"/> </p>
-    
-    </div>
-    
-    </div>
+
+
+
+
+
    {/* to be components  */}
 
-   <div className="grid grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4 ">
-  
-    <div className=""><img src={user} alt="housng img" className="h-50 rounded-lg  p-4 "/></div>
-    {/* // need to hace a fall back square - empty container that says choose img */}
-    
-    <div className=" p-4 overflow-auto max-h-50">
-      <h1>Owner: Kenny Smith</h1>
-      <h2>Addi: Tavi </h2>
-      <h2>Costs: $4 </h2>
-      <p>notes: <Input placeholder="lorem"/> </p>
-    
-    </div>
-    
-    </div>
-   
-   <div className="grid grid-cols-[200px_auto] bg-zinc-300 rounded-lg m-4 ">
-  
-    <div className=""><img src={user} alt="housng img" className="h-50 rounded-lg  p-4 "/></div>
-    {/* // need to hace a fall back square - empty container that says choose img */}
-    
-    <div className=" p-4 overflow-auto max-h-50">
-      <h1>Owner: Kenny Smith</h1>
-      <h2>Addi: Tavi </h2>
-      <h2>Costs: $4 </h2>
-      <p>notes: <Input placeholder="lorem"/> </p>
-    
-    </div>
-    
-    </div>
-   </>
+     </>
   )
 }
 
 export default NavBar
+
+
+// notes : the display should be editable  on click so no forms - click and ten you can change it without moving off screen 
+// create an add screen -- form + change these out to cards -- display 
